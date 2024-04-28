04c34863dc52c943c88b79920b63db51089bd29b
Mohammed Malleck <mohammedmalleck@gmail.com>
2024-04-28 18:46:33 +0400

    Version 4

---
+++ renderBikeHTML.js
@@ -0,0 +1,61 @@
+//bike section JS CODE
+export let bikes = [
+  {
+    id : 1
+  },
+  {
+    id : 2
+  },
+  {
+    id : 3
+  },
+  {
+    id : 4
+  },
+  {
+    id : 5
+  },
+  {
+    id : 6
+  },
+  {
+    id : 7
+  },
+  {
+    id : 8
+  }
+];
+
+export function renderBikesHTML(){
+
+let html = '';
+
+bikes.forEach((bikeEl)=>{
+
+  html += 
+  `
+    <div class="bike-product-container">
+      <div class="free-cancellation-text">
+        Free Cancellation
+      </div>
+      <img src="images/bike-${bikeEl.id}.png" class="bike-product-image">
+      <div class="bike-product-info-container">
+        <div>
+          <div class="bike-product-name">BMW R1300GS</div>
+          <div class="adventure-text">Adventure</div>
+        </div>
+        <div>
+          <div class="bike-rent-info-container">
+            <div class="price">$17</div>
+            <div class="time">/hour</div>
+          </div>
+        </div>
+      </div>
+      <button class="book-bike-btn">Book Bike</button>
+    </div>
+  `;
+
+});
+
+document.querySelector('.bike-products-container').innerHTML = html;
+}
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import { renderBikesHTML , bikes } from "./renderBikeHTML.js";
+
+let productPushed;
+
+//sidebar JS CODE
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const sidebarEl = document.querySelector('.sidebar');
 hamburgerIcon.addEventListener('click',()=>{
@@ -7,4 +12,31 @@ hamburgerIcon.addEventListener('click',()=>{
   }else{
     sidebarEl.classList.add('show');
   }
-})
+});
+
+renderBikesHTML();
+addANewProduct();
+
+window.addEventListener('resize',addANewProduct);
+
+function addANewProduct(){
+const screenWidth = window.innerWidth;
+const breakpointOne = 910;
+const breakpointTwo = 700;
+
+if(screenWidth < breakpointOne && screenWidth > breakpointTwo){
+  if(!productPushed){
+    bikes.push({id : 9});
+    renderBikesHTML();
+    productPushed = true;
+  }
+}else{
+  if(productPushed){
+    const lastIndex = bikes.length - 1;
+    bikes.splice(lastIndex,1);
+    renderBikesHTML();
+    productPushed = false;
+  }
+}
+
+}
